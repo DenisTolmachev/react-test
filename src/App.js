6fa5e646d396707ee0c75214916aac2c241d007d
Denis <denis.tolmachev@gmail.com>
2022-06-22 21:21:32 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactDOM from "react-dom";
 
-function App() {
+const imageUrl =
+  "https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640";
+const price = 10.99;
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <img src={imageUrl} alt="Tacos With Lime" width="640" />
+      <h2>Tacos With Lime</h2>
+      <p>Price: {price}$</p>
+      <button type="button">Add to cart</button>
     </div>
   );
-}
+};
 
 export default App;
